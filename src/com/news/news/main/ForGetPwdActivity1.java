9631f7683fc37861afc54dbb77f3cb3862977101
Administrator <Administrator@MS-20160630IAIT>
2016-09-26 09:44:58 +0800

    2016-9-25

--- src/com/news/news/main/ForGetPwdActivity1.java
+++ src/com/news/news/main/ForGetPwdActivity1.java
@@ -29,13 +29,13 @@ public class ForGetPwdActivity1 extends Activity implements View.OnClickListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpwd1);
-        //设置标题栏--start
-        //标题栏开始
+        //设置标题�?-start
+        //标题栏开�?
         title_mid=(TextView)findViewById(R.id.title_mid);
         title_mid.setText("确认用户信息");
         title_left=(ImageView)findViewById(R.id.title_left);
         title_left.setVisibility(View.VISIBLE);
-        //标题栏结束
+        //标题栏结�?
         title_left.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -43,7 +43,7 @@ public class ForGetPwdActivity1 extends Activity implements View.OnClickListener
                 finish();
             }
         });
-        //设置标题栏--end
+        //设置标题�?-end
         btnAchieve = (Button) findViewById(R.id.fgpwd_btn_achieve);
         etUsername = (EditText) findViewById(R.id.fgpwd_et_username);
         etClass = (EditText) findViewById(R.id.fgpwd_et_class);
@@ -57,7 +57,7 @@ public class ForGetPwdActivity1 extends Activity implements View.OnClickListener
         String classs = etClass.getText().toString().trim();
         long a=-1;
         if (username.equals("")){
-            Toast.makeText(getApplicationContext(), "用户名不能为空", Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "用户名不能为�?", Toast.LENGTH_SHORT).show();
         }else{
             RegLogin_Server server=new RegLogin_Server(getApplicationContext());
             a=server.forGetPwd(username,classs);
