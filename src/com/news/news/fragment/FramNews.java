51425341e655b4337d3c5fd101411cf9e63ef5c4
Ark <Ark@danei-PC>
2016-09-28 17:38:26 +0800

    w

--- src/com/news/news/fragment/FramNews.java
+++ src/com/news/news/fragment/FramNews.java
@@ -1,7 +1,14 @@
 package com.news.news.fragment;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import org.xutils.x;
+import org.xutils.view.annotation.ViewInject;
+
 import com.news.news.R;
+import com.news.news.fragment.news.NewsFragment;
+import com.news.news.fragment.news.ToutiaoFragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,72 +20,68 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class FramNews extends Fragment {
+	@ViewInject(R.id.vp_news_framents)
 	private ViewPager vpFraments;
+	@ViewInject(R.id.rg_news)
 	private RadioGroup rgNews;
-	private RadioButton rbRedian; // 热点
+	@ViewInject(R.id.rb_news_toutiao)
 	private RadioButton rbToutiao; // 头条
+	@ViewInject(R.id.rb_news_keji)
 	private RadioButton rbKeji; // 科技
+	@ViewInject(R.id.rb_news_yule)
 	private RadioButton rbYule; // 娱乐
-	private RadioButton rbYouxi; // 游戏
-	private RadioButton rbYingshi; // 影视
+	@ViewInject(R.id.rb_news_tiyu)
 	private RadioButton rbTiyu; // 体育
+	@ViewInject(R.id.rb_news_caijing)
 	private RadioButton rbCaijing; // 财经
-	private RadioButton rbQiche; // 汽车
+	@ViewInject(R.id.rb_news_shishang)
 	private RadioButton rbShishang; // 时尚
+	@ViewInject(R.id.rb_news_junshi)
 	private RadioButton rbJunshi; // 军事
-	private RadioButton rbLishi; // 历史
-	private RadioButton rbCaipiao; // 彩票
-	private RadioButton rbLuntan; // 论坛
-	//新闻Frag
+	@ViewInject(R.id.rb_news_shehui)
+	private RadioButton rbShehui; // 社会
+	@ViewInject(R.id.rb_news_guonei)
+	private RadioButton rbGuonei; // 国内
+	@ViewInject(R.id.rb_news_guoji)
+	private RadioButton rbGuoji; // 国际
+
+	// 新闻Frag
 	private List<Fragment> newsFragments;
 	private View view;
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		if(view==null){
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		if (view == null) {
 			view = inflater.inflate(R.layout.fram_news, null);
-			// 初始化
-			setViews(view);
+			// x注解
+			x.view().inject(this, view);
 			// FragmentAdapter
 			setFragmentAdapter();
 			// 监听器
 			setListeners();
-
-
-		}else{
-			((ViewGroup) view.getParent()).removeView(view); 
+		} else {
+			((ViewGroup) view.getParent()).removeView(view);
 		}
 		return view;
 	}
 
 	private void setFragmentAdapter() {
-		newsFragments=new ArrayList<Fragment>();
+		newsFragments = new ArrayList<Fragment>();
+		newsFragments.add(new ToutiaoFragment());
+		newsFragments.add(new NewsFragment("shehui"));
+		newsFragments.add(new NewsFragment("guonei"));
+		newsFragments.add(new NewsFragment("guoji"));
+		newsFragments.add(new NewsFragment("yule"));
+		newsFragments.add(new NewsFragment("tiyu"));
+		newsFragments.add(new NewsFragment("junshi"));
+		newsFragments.add(new NewsFragment("keji"));
+		newsFragments.add(new NewsFragment("caijing"));
+		newsFragments.add(new NewsFragment("shishang"));
+		
+
 	}
 
 	private void setListeners() {
-
-	}
-
-	private void setViews(View view) {
-		// ViewPager
-		vpFraments = (ViewPager) view.findViewById(R.id.vp_news_framents);
-
-		// Radio
-		rgNews = (RadioGroup) view.findViewById(R.id.rg_news);
-		rbRedian = (RadioButton) view.findViewById(R.id.rb_news_redian);
-		rbToutiao = (RadioButton) view.findViewById(R.id.rb_news_toutiao);
-		rbKeji = (RadioButton) view.findViewById(R.id.rb_news_keji);
-		rbYule = (RadioButton) view.findViewById(R.id.rb_news_yule);
-		rbYouxi = (RadioButton) view.findViewById(R.id.rb_news_youxi);
-		rbYingshi = (RadioButton) view.findViewById(R.id.rb_news_yingshi);
-		rbTiyu = (RadioButton) view.findViewById(R.id.rb_news_tiyu);
-		rbCaijing = (RadioButton) view.findViewById(R.id.rb_news_caijing);
-		rbQiche = (RadioButton) view.findViewById(R.id.rb_news_qiche);
-		rbShishang = (RadioButton) view.findViewById(R.id.rb_news_shishang);
-		rbJunshi = (RadioButton) view.findViewById(R.id.rb_news_junshi);
-		rbLishi = (RadioButton) view.findViewById(R.id.rb_news_lishi);
-		rbCaipiao = (RadioButton) view.findViewById(R.id.rb_news_caipiao);
-		rbLuntan = (RadioButton) view.findViewById(R.id.rb_news_luntan);
 
 	}
 }
